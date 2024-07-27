da456ddbf741a2fd5f50e1379a5b6e92259f21b9
Mateusz Szymanski <mszymanski027@gmail.com>
2024-07-28 00:02:02 +0200

    edit js

--- 44 social-media-dashboard-with-theme-switcher-master/app.js
+++ 44 social-media-dashboard-with-theme-switcher-master/app.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const themeSwitcher = document.getElementById("theme-switcher");
 
+  document.body.classList.add("dark-mode");
+
   themeSwitcher.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
   });
