f57c245fe84ff210d028d3d329844f82e9be9633
dezmerek <mszymanski027@gmail.com>
2024-11-25 19:31:55 +0100

    Update stats display and progress bar functionality with dynamic values and responsive adjustments

--- 53 crowdfunding-product-page-main/app.js
+++ 53 crowdfunding-product-page-main/app.js
@@ -1,6 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerIcon = document.getElementById("hamburger");
   const menu = document.getElementById("menu");
+  let ofTotal = document.getElementById("ofTotal");
+  let totalBackers = document.getElementById("totalBackers");
+  let daysLeft = document.getElementById("daysLeft");
+  let barProgress = document.getElementById("barProgress");
+  let BambooLeft = document.getElementById("BambooLeft");
+  let BlackEditionLeft = document.getElementById("BlackEditionLeft");
+  let MahoganyLeft = document.getElementById("MahoganyLeft");
+
+  let ofTotalNumber = 89914;
+  let totalBackersNumber = 5007;
+  let daysLeftNumber = 56;
+  let barProgressNumber = (ofTotalNumber / 100000) * 100;
+  let BambooLeftNumber = 101;
+  let BlackEditionLeftNumber = 64;
+  let MahoganyLeftNumber = 0;
+
+  ofTotal.innerHTML = "$" + ofTotalNumber.toLocaleString();
+  totalBackers.innerHTML = totalBackersNumber.toLocaleString();
+  daysLeft.innerHTML = daysLeftNumber;
+  barProgress.style.width = barProgressNumber + "%";
+  BambooLeft.innerHTML = BambooLeftNumber;
+  BlackEditionLeft.innerHTML = BlackEditionLeftNumber;
+  MahoganyLeft.innerHTML = MahoganyLeftNumber;
+
+  console.log(barProgressNumber);
 
   hamburgerIcon.addEventListener("click", () => {
     menu.classList.toggle("active");
